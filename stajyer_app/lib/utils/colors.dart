37b5a84b12c7230dcabf56115ab987260a0b01b8
hbhdr <bhdrarin@gmail.com>
2024-07-29 12:21:20 +0300

    colors eklendi

---
+++ stajyer_app/lib/utils/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+Color appbar = Color.fromARGB(255, 0, 47, 52);
+Color ilanCard = Color.fromARGB(255, 0, 135, 117);
+Color GirisCard = Color.fromARGB(255, 219, 226, 239);
+Color button = Color.fromARGB(255, 0, 51, 102);
+Color text = Color.fromARGB(255, 0, 51, 102);
