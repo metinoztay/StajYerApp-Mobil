ba77f92cce343576815532f4a7dfbf0d493c8fac
metinoztay <metinoztay@gmail.com>
2024-08-08 14:38:11 +0300

    Project cleaned

--- stajyer_app/lib/utils/colors.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-Color appbar = Color.fromARGB(255, 0, 47, 52);
-Color ilanCard = Color.fromARGB(255, 0, 135, 117);
-Color GirisCard = Color.fromARGB(255, 219, 226, 239);
-Color button = Color.fromARGB(255, 0, 51, 102);
-Color text = Color.fromARGB(255, 0, 51, 102);
-Color background = Color.fromARGB(255, 219, 226, 239);
