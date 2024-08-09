d97add1f8eddd06734696920506dbea03c82d72d
hbhdr <bhdrarin@gmail.com>
2024-08-09 15:42:06 +0300

    endpoint eklendi

--- stajyer_app/lib/Company/services/Endpoints.dart
+++ stajyer_app/lib/Company/services/Endpoints.dart
@@ -6,4 +6,7 @@ class Endpoints {
   static const String ListTaxOfficeByCityIdUrl = '$baseUrl/City/ListTaxOffices';
   static const String ListAdvertByCompanyUserId =
       '$baseUrl/Advert/ListAdvertsByCompanyUserId';
+
+    static const String complogin = '$baseUrl/CompanyUser/Login';
+
 }
